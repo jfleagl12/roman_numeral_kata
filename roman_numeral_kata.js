02669a1ac37bba8b6b308783ebca3fbf4c31d784
Jason Fleagle <jasonfleagle@hotmail.com>
2015-11-24 19:46:51 +0000

    Modified the arrangement and comments

--- roman_numeral_kata.js
+++ roman_numeral_kata.js
@@ -1,6 +1,7 @@
 var convert = function(num) {
 
-  // Create arrays with default conversion with matching indices.
+  // The two arrays with default conversion with matching indices.
+  
   var decimalValue = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
   var romanNumeral = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
 
@@ -8,11 +9,14 @@ var convert = function(num) {
 
   var romanized = '';
 
-  // Loop through the indices of the decimalValue array.
+  // Below loops through the indices of the decimalValue array.
+  
   for (var index = 0; index < decimalValue.length; index++) {
-    // Continue to loop while the value at the current index will fit into numCopy
+    //  The loop continues only if the value at the current index will fit into numCopy
+    
     while (decimalValue[index] <= num) {
-      // Add the Roman numeral & decrease numCopy by the decimal equivalent.
+      // This adds the Roman numeral & decreases numCopy by the decimal equivalent.
+      
       romanized += romanNumeral[index];
       num -= decimalValue[index];
     }
